2669f84df2388602cadf907c6ff079228b476f3b
ctrl-alt-del <jiyang881230@gmail.com>
2014-10-08 23:04:47 -0700

    update SearchActivity;

--- src/com/htexercise/SearchActivity.java
+++ src/com/htexercise/SearchActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 
@@ -23,13 +24,17 @@ public class SearchActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+
+		switch (item.getItemId()) {
+		case R.id.action_settings:
 			return true;
+		case R.id.action_search:
+			Toast.makeText(this, "Search is click..", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
