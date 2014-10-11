5af2b619543e8fa10f19e37417089fad9ede9326
ctrl-alt-del <jiyang881230@gmail.com>
2014-10-11 15:19:49 -0700

    implement OnClickListener on PlaceDetailsActivity;

--- src/com/htexercise/PlaceDetailsActivity.java
+++ src/com/htexercise/PlaceDetailsActivity.java
@@ -1,15 +1,19 @@
 package com.htexercise;
 
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PlaceDetailsActivity extends Activity {
+public class PlaceDetailsActivity extends Activity implements OnClickListener {
 
 	private TextView address;
 	private TextView location;
 	private TextView placeId;
+	private Button cancel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,18 @@ public class PlaceDetailsActivity extends Activity {
 		this.address = (TextView) this.findViewById(R.id.place_details_address);
 		this.location = (TextView) this.findViewById(R.id.place_details_location);
 		this.placeId = (TextView) this.findViewById(R.id.place_details_place_id);
-
+		this.cancel = (Button) this.findViewById(R.id.place_details_cancel);
+		
+		this.cancel.setOnClickListener(this);
 	}
 
-
-	
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.place_details_cancel:
+			this.onBackPressed();
+		default:
+			break;
+		}
+	}
 }
