a069cd375784b6c083354516e91c86e75fd465d0
ctrl-alt-del <jiyang881230@gmail.com>
2015-12-04 21:50:46 -0800

    added solution;

--- abb-java/src/com/abb/decoder/Main.java
+++ abb-java/src/com/abb/decoder/Main.java
@@ -1,5 +1,9 @@
 package com.abb.decoder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     /**
@@ -8,6 +12,30 @@ public class Main {
      * Ex: given "abc", its completed list of variants are "Abc", "ABc", "ABC", "AbC", "aBc", "aBC", "abC
      */
     public static void main(String[] args) {
-	// write your code here
+        String str = "abc";
+        for (String result : unveilCode(str.toCharArray(), 0)) {
+            System.out.println(result);
+        }
+    }
+
+    public static List<String> unveilCode(char[] chars, int n) {
+        int l = chars.length;
+        char lastChar = chars[l - 1];
+        if (Character.toUpperCase(lastChar) == lastChar) {
+            return Collections.emptyList();
+        }
+
+        char tempChar;
+        List<String> results = new ArrayList<>();
+        for (int i = n; i < l; i++) {
+            tempChar = chars[i];
+
+            chars[i] = Character.toUpperCase(chars[i]);
+            results.add(new String(chars));
+            results.addAll(unveilCode(chars, i + 1));
+
+            chars[i] = tempChar;
+        }
+        return  results;
     }
 }
