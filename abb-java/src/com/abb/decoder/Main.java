48dbfcafe5f2b48a38198a564d750822c5a4f878
ctrl-alt-del <jiyang881230@gmail.com>
2015-12-04 21:44:21 -0800

    added problem description;

--- abb-java/src/com/abb/decoder/Main.java
+++ abb-java/src/com/abb/decoder/Main.java
@@ -2,6 +2,11 @@ package com.abb.decoder;
 
 public class Main {
 
+    /**
+     * Given a string, find all its possible variants that includes at least one capitalized letter
+     *
+     * Ex: given "abc", its completed list of variants are "Abc", "ABc", "ABC", "AbC", "aBc", "aBC", "abC
+     */
     public static void main(String[] args) {
 	// write your code here
     }
